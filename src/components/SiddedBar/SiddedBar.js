a175712ec5b314fc1024010ec1abc9202754f84d
Lucas Suzin Bertan <bertan1707@gmail.com>
2021-03-24 22:23:50 -0300

    Update SiddedBar.js

--- src/components/SiddedBar/SiddedBar.js
+++ src/components/SiddedBar/SiddedBar.js
@@ -3,6 +3,11 @@ import React from 'react';
 //importing styled-components for styling
 import styled from 'styled-components';
 
+//importing elements
+import Logo from './components/Logo';
+import Navigation from './components/Navigation';
+import Favorites from './components/Favorites';
+
 function SiddedBar() {
 
     /*
@@ -12,13 +17,22 @@ function SiddedBar() {
     const Container = styled.div`
     display: flex;
     flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
     height: 100vh;
     width: 344px;
     border-right: 1px solid #bdbdbd;
     background-color: #fff;
+    padding-bottom: 48px;
+    padding-top: 48px;
+    box-sizing: border-box;
     `
     return (
-        <Container></Container>
+        <Container>
+            <Logo></Logo>
+            <Navigation></Navigation>
+            <Favorites></Favorites>
+        </Container>
     );
 }
 
