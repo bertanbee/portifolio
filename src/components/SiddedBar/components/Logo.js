f847038f5b6db71e182a3ef209b4633374d36917
Lucas Suzin Bertan <bertan1707@gmail.com>
2021-03-27 17:14:44 -0300

    Update Logo.js

--- src/components/SiddedBar/components/Logo.js
+++ src/components/SiddedBar/components/Logo.js
@@ -13,18 +13,30 @@ function Logo() {
     */
    const LogoImage = styled.img`
    @media only screen and (max-width: 1000px) {
+    padding-top: 12px;
+    opacity: 0.8;
     height: 60px;
     width: 60px;
+    border-radius: 100%;
+    transition: 0.25s;
+    :hover {
+        cursor: pointer;
+        opacity: 0.9;
+        transition: 0.25s;
+    }
    }
-   margin-top: -45px;
-   opacity: 0.8;
-   border-radius: 100%;
-   transition: 0.25s;
-   :hover {
-       cursor: pointer;
-       opacity: 0.9;
-       transition: 0.25s;
+   @media only screen and (min-width: 1000px) {
+    margin-top: -45px;
+    opacity: 0.8;
+    border-radius: 100%;
+    transition: 0.25s;
+    :hover {
+        cursor: pointer;
+        opacity: 0.9;
+        transition: 0.25s;
+    }
    }
+   
    `
 
     return (
